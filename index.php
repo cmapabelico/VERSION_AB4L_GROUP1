6c045a71ca8e04ea664e693f4fd3b8c7f15c6aa3
unknown <daryllft19@gmail.com>
2013-02-27 14:15:07 +0800

    gumaganang carousel

--- index.php
+++ index.php
@@ -31,7 +31,6 @@
 				$_SESSION["id"] = $email; //user id
 				$_SESSION["tray"] = array(); //user tray
 				$_SESSION["traycontents"] = 0; //number of items in tray
-				$_SESSION["subtotal"] = 0; //subtotal of ordered items
 				header("Location: home.php");
 			}
 			else{
@@ -42,11 +41,13 @@
 	}
 	
 ?>
+<!DOCTYPE html>
 <html>
-
 <head>
 	<title>BRGR: The Burger Project Online</title>
 	<link rel="stylesheet" href="style.css" type="text/css"/>
+	<link rel="stylesheet" href="bootstrap.css/bootstrap.min.css" type="text/css"/>
+	<link rel="stylesheet" href="bootstrap.css/bootstrap-responsive.min.css" type="text/css"/>
 </head>
 
 <body>
@@ -58,7 +59,29 @@
 	<img src="images/logo.png" width="500"/><br/><br/>
 	
 	<!--FLASH-->
-	<div class="flash">Flash/JCarousel thing here</div><br/>
+	<div class="flash">
+			<div id="myCarousel" class="carousel slide">
+			  <ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			  </ol>
+			  <!-- Carousel items -->
+			  <div class="carousel-inner">
+				<div class="active item"><img src="http://www.yadig.com/uploads/biz-logo/burger_ph_alb_091020114032.jpg" alt></div>
+				<div class="item"><img src="http://upload.wikimedia.org/wikipedia/commons/9/91/Burger_King_Whopper_Combo.jpg" alt></div>
+				<div class="item"><img src="http://images4.fanpop.com/image/photos/23800000/BK-burger-king-23892437-263-336.jpg" alt></div>
+			  </div>
+			  <!-- Carousel nav -->
+			  <a class="carousel-control left" href="#myCarousel" data-slide="prev"><</a>
+			  <a class="carousel-control right" href="#myCarousel" data-slide="next">></a>
+			</div>
+	</div>
+	
+	<br/>
+	<br/>
+	<br/>
+	<br/>
 	
 	<!--DESCRIPTION-->
 	What do you want in your burger? Create your own at "BRGR: The Burger Project!" <br/><br/>
@@ -73,7 +96,11 @@
 	<a href="register.php">Register</a> <a href="home.php">Proceed as guest</a>
 	
 	</center>
-
+	
+	
+	<script src="bootstrapjs/jquery-1.9.1.js"></script>
+	<script src="bootstrapjs/bootstrap-carousel.js"></script>
+	<script src="bootstrapjs/bootstrap.min.js"></script>
 </body>
 
 </html>
